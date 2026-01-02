e67e2505fc2b46dc3ec7bcf7cc0c9da882ad8a27
developer-amna <hafizaamna.webdev@gmail.com>
2026-01-02 19:07:03 -0300

    Next Greater Element

--- DSA/Stack.cpp
+++ DSA/Stack.cpp
@@ -437,3 +437,37 @@ int main(){
 	cout<<endl;
 	return 0;
 }
+
+\\ Program 10
+
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+int main(){
+		int arr_raw[] = {6, 8, 0, 1, 3};
+        vector<int> arr(arr_raw, arr_raw + 5);   // 5 elements
+	
+	
+	// Next Greater Element
+	stack<int>s;
+	vector<int>ans(arr.size(),0);
+	for(int i=arr.size()-1; i>=0; i--){
+		while(s.size()>0 && s.top()<=arr[i]){
+			s.pop();
+		}
+		if(s.empty()){
+			ans[i] = -1;
+		} else {
+			ans[i] = s.top();
+		}
+		s.push(arr[i]);
+	}
+	for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+}
+
+	cout<<endl;
+	
+	return 0;
+}
