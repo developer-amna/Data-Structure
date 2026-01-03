093f1c3d91c68003389f28fbaea2cf00176fd760
developer-amna <hafizaamna.webdev@gmail.com>
2026-01-03 14:26:31 -0300

    Previous Smaller Element

--- DSA/Stack.cpp
+++ DSA/Stack.cpp
@@ -471,3 +471,43 @@ int main(){
 	
 	return 0;
 }
+
+// Program # 11
+
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+	vector<int>preSmallerElement(vector<int>arr) {
+		vector<int>ans(arr.size(),0);
+		stack<int>s;
+		
+		for(int i=0; i<arr.size(); i++) {
+			while(s.size()>0 && s.top()>=arr[i]) {
+				s.pop();
+			}
+			
+			if(s.empty()) {
+				ans[i] = -1;
+			} else {
+				ans[i] = s.top();
+			}
+			
+			s.push(arr[i]);
+		}
+		return ans;
+	}
+	int main() {
+		int arr_raw[] = {3,1,0,8,6};
+		vector<int>arr(arr_raw, arr_raw + 5);
+		
+		vector<int>ans=preSmallerElement(arr);
+			for (int i = 0; i < ans.size(); i++) {
+                    cout << ans[i] << " ";
+}
+
+	cout<<endl;
+	
+	return 0;
+	}
+	
