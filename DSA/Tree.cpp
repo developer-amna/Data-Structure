a3638a2e095cb6fcd8576ffb3b40d4ed80d3981c
developer-amna <hafizaamna.webdev@gmail.com>
2026-01-19 08:51:13 -0300

    Inorder Traversal

--- DSA/Tree.cpp
+++ DSA/Tree.cpp
@@ -130,3 +130,57 @@ int main() {
 
     return 0;
 }
+
+// Program # 3
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+
+Node* buildTree(int preorder[], int n) {
+    idx++;
+    if (idx >= n || preorder[idx] == -1) {
+        return NULL;
+    }
+    Node* root = new Node(preorder[idx]);
+    root->left = buildTree(preorder, n);
+    root->right = buildTree(preorder, n);
+    return root;
+}
+
+// inorder
+void inOrder(Node* root) {
+	if (root == NULL) {
+		return;
+	}
+	inOrder(root->left);
+	cout << root->data << " ";
+	inOrder(root->right);
+}
+
+int main() {
+    int preorder[] = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    int n = sizeof(preorder) / sizeof(preorder[0]);
+
+    Node* root = buildTree(preorder, n);
+
+    
+    
+    inOrder(root);
+    cout << endl;
+
+    return 0;
+}
