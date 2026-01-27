d00d6c2a8f14a43e48d1f23c572649520c1ed93d
developer-amna <hafizaamna.webdev@gmail.com>
2026-01-27 21:31:27 +0500

    Selection sort

--- DSA/Sorting.cpp
+++ DSA/Sorting.cpp
@@ -1,3 +1,6 @@
+// Program # 1
+        // Bubble Sort
+
 #include<iostream>
 using namespace std;
 class Bubble {
@@ -31,6 +34,54 @@ void Bubble::print(void) {
 }
 int main {
       Bubble obj;
+      obj.input();
+      obj.sort();
+      obj.print();
+      return 0;
+}
+
+// Program # 2
+        // Selection Sort
+
+#include<iostream>
+using namespace std;
+class Selection {
+	private:
+		int abc[5];
+	public:
+		void input(void);
+		void sort(void);
+		void print(void);
+};
+void Selection::input(void) {
+	cout << "Enter 5 values " << endl;
+	for(int i=0;i<=4;i++)
+	           cin >> abc[i];
+}
+void Selection::sort(void) {
+	int mini,temp,loc;
+	for(int u=0;u<4;u++) {
+		mini = abc[u];
+		loc = u;
+		for (int i=u;i<=4;i++)
+		    if (mini > abc[i]) {
+		    	mini = abc[i];
+		    	loc = i;
+		    }
+		    
+		temp = abc[loc];
+		abc[loc] = abc[u];
+		abc[u] = temp;
+	}
+}
+void Selection::print(void) {
+	cout << "Sorted Array " << endl;
+	for(int i=0;i<=4;i++)
+	    cout << abc[i] << "\t";
+	cout << endl;
+}
+int main() {
+      Selection obj;
       obj.input();
       obj.sort();
       obj.print();
